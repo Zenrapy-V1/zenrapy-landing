2d72a9927c84c9e14d0061f7ad8a979654a98f6f
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-01-21 20:37:31 +0100

    Hero Update

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import "./index.scss"
 import { BsChevronDown } from 'react-icons/bs'
-import HeroImage from "../../assets/heroImage2.png"
+import HeroImage from "../../assets/heroImage1.png"
 import HeroBg2 from "../../assets/heroBg2.png"
+import HeroBg4 from "../../assets/heroBg4.png"
 import MobileImage from "../../assets/heroImage2.png"
+import heroTick from "../../assets/hero-tick.png";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+
 
 
 const HeroSection = () => {
@@ -12,25 +15,38 @@ const HeroSection = () => {
     return (  
         <div className='hero'>
            <div className='left'>
-            <h3>Premium Health <br/> Massage from <br/> professional Therapists</h3>
-            <div className='left-text'>
-                <p>
-                    We aim to connect you to the best top talented massage 
-                    therapists anywhere around the world to ease your pain and
-                    give you a topnotch relaxation.
-                </p>
-                <button 
-                onClick={(e) => {e.preventDefault(); window.location.replace('/#launch');}}
-                >
-                    Join the waitlist
+                <h3> 
+                    <span className='hero-sub-color'>Premium</span> Health <br/> Massage from <br/> professional Therapists
+                </h3>
+                <div className='left-text'>
+                    <ul>
+                        <li className='hero-list'>
+                            Get Massaged by Top and Talented Massage Therapists <br/> around your locality.
+                            <img className='hero-tick' src={heroTick} alt="" />
+                        </li>
+                        <li className='hero-list'>
+                            Licensed professionals that heals the pain in your <br/> body and help you reduce stress?
+                            <img className='hero-tick' src={heroTick} alt="" />
+                        </li>
+                        <li className='hero-list'>
+                            Makes your search for real massage ends?
+                            <img className='hero-tick' src={heroTick} alt="" />
+                        </li>
+                        <li className='hero-list'>
+                            Leave it to us we got you covered, we built a uber <br/> for masseurs
+                        </li>                          
+                    </ul>
+                </div>
+                <button className='hero-button' onClick={(e) => {e.preventDefault(); window.location.replace('/#launch');}}>
+                    Book a session
                 </button>
-            </div>
            </div>
             <AnimationOnScroll animateIn="animate__bounceIn" initiallyVisible="true">
               <div className='right'>
                 <img className='main-image' src={HeroImage} alt="" /> 
                 <img className="mobile-img" src={MobileImage} />
                 <img className='elipse' src={HeroBg2} alt="" />
+                <img className='sparkle' src={HeroBg4} alt="" />
             </div>
            </AnimationOnScroll>
             
