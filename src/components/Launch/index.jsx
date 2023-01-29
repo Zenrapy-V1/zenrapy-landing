e538ae7dfe7c55947dc546a8d211bcc79d9b755c
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-01-29 08:19:29 +0100

    Update on landing

--- src/components/Launch/index.jsx
+++ src/components/Launch/index.jsx
@@ -1,81 +1,43 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import SubmitMark from "../../assets/submitMark.png"
 import "./index.scss"
-import { BsChevronDown } from 'react-icons/bs'
-import axios from "axios"
-import {useNavigate} from 'react-router-dom';
-import FlashMessage from "../../pages/layout/landingFlash"
-import FlashMessageError from "../../pages/layout/flashMessage"
+import feelBetterSquare2 from "../../assets/feelBetterSquare2.png"
+
+const gettingStarted = () => {
+
+    return (  
+        <div>
+            <p className='heading'>Want to feel Better Instantly?</p>
+            <div className="feel-better reverse-illustration section__padding" id="possibility">
+                <div class="container">
+                  <div class="card-wrapper">
+                    <div class="card-img">
+                      <img src={feelBetterSquare2} alt="possibility"/>
+                    </div>
+                    <div class="card-body">
+                      <h5>Now is the time 🏖</h5>
+                      <p>
+                        Rest assured! You can't go wrong with Zenrapy
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="content-text">
+                  <div className='context'>
+                      <p>
+                          Book a relaxing deep tissue, sports, or Swedish<br/> 
+                          massage to relieve stress and pain while<br/> 
+                          improving circulation and healthy body
+                      </p>
+                      <a target='_blank' href={'https://wa.me/message/YNQGXF43I7LEA1'}>
+                          <button  className='context-button'>Book a session</button>
+                      </a>
+                  </div>
+                </div>
+            </div>
 
 
-const Launch = ({modalShow, setModalShow, errorModalShow, setErrorModalShow}) => {
-    const [success, setSuccess] = useState("false")
-    const [error, setError] = useState("false")
-    const [email, setEmail] = useState("")
-    const [join, setJoin] = useState("Join waitlist")
-    const navigate = useNavigate();
-
-    console.log(email)
-
-    const handleJoin = () => {
-      setJoin("Joining...")
-    }
-
- const submitForm = async (e) => {
-    e.preventDefault();
-    const emailData = {
-      email   
-     }
-
-   axios.post("https://zenrapy.up.railway.app/landing_page/newsletter_subscription/",  emailData,{
-      headers: {
-        'Content-Type': 'application/json'
-    }
-    }).then((response) => {
-     setEmail("");
-    //  setModalShow(true)
-     setJoin("Join waitlist")
-     setSuccess(true)
-        
-    }).catch(error => {
-      console.log(error)
-      // setErrorModalShow(true)
-      setJoin("Join waitlist")
-      setError(true)
-    })    
-    
-   }
-
-    return (
-        <>  
-        <div id="launch" className='launch'>
-           <h2>WE LAUNCH SOON!!!! 🥳</h2>
-           <p>Sign up to join our waitlist and get a free massage session when we launch</p>
-           <p>Also get amazing discounts and be updated about our journey before we launch</p>
-           <form className='join-form' onSubmit={submitForm}>
-            <input 
-            name='email' 
-            value={email} 
-            type="text" 
-             onChange={(e)=>setEmail(e.target.value)}
-            placeholder='Input mail address'
-            />
-            <button
-            type="submit" 
-            name="submit"
-            onClick={handleJoin}
-            >{join}</button>
-           </form>
         </div>
-        {
-          success === true ? <FlashMessage color="warning" message="We are glad to have you!!! 🥳" /> : ""
-        }
-        {
-          error === true ? <FlashMessageError color="warning" message="Whoops!!! There's an error, Please try again" /> : ""
-        }
-        </>
     );
 }
  
-export default Launch;
+export default gettingStarted;
