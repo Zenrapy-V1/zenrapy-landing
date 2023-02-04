135e29f8a201bdd2d69a4383bf589e2bad38abd3
unknown <ademolababalola2003@gmail.com>
2023-02-04 17:46:02 +0100

    updated hero section

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,55 +1,67 @@
-import React, { useState } from 'react'
-import "./index.scss"
-import HeroImage from "../../assets/heroImage1.png"
-import MobileImage from "../../assets/heroImage2.png"
-import mainHome from "../../assets/mainHome.png"
-import mainHomeMobile from "../../assets/mainHomeMobile.png"
-import HeroBg2 from "../../assets/heroBg2.png"
-import HeroBg4 from "../../assets/heroBg4.png"
+import React, { useState } from "react";
+import "./index.scss";
+import HeroImage from "../../assets/heroImage1.png";
+import MobileImage from "../../assets/heroImage2.png";
+import mainHome from "../../assets/mainHome.png";
+import mainHomeMobile from "../../assets/mainHomeMobile.png";
+import HeroBg2 from "../../assets/heroBg2.png";
+import HeroBg4 from "../../assets/heroBg4.png";
 import heroTick from "../../assets/hero-tick.png";
-import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const HeroSection = () => {
-    return (  
-        <div className='hero'>
-           <div className='left'>
-                <h3> 
-                    <span className='hero-sub-color'>Premium</span> Health <br/> Massage from <br/> professional Therapists
-                </h3>
-                <div className='left-text'>
-                    <ul>
-                        <li className='hero-list'>
-                            Get Massaged by Top and Talented Massage Therapists <br/> around your locality.
-                            <img className='hero-tick' src={heroTick} alt="" />
-                        </li>
-                        <li className='hero-list'>
-                            Licensed professionals that heals the pain in your <br/> body and help you reduce stress?
-                            <img className='hero-tick' src={heroTick} alt="" />
-                        </li>
-                        <li className='hero-list'>
-                            Makes your search for real massage ends?
-                            <img className='hero-tick' src={heroTick} alt="" />
-                        </li>
-                        <li className='hero-list'>
-                            Leave it to us we got you covered, we built a uber <br/> for masseurs
-                        </li>                          
-                    </ul>
-                </div>
-                <a target='_blank' href={'https://wa.me/message/YNQGXF43I7LEA1'}>
-                    <button  className='home-hero-button'>Book a session</button>
+  return (
+        <div className="hero">
+          <div className="left">
+            <h3>
+              <span className="hero-sub-color">Premium</span> Health <br /> Massage
+              from <br /> professional Therapists
+            </h3>
+            <div className="left-text">
+              <ul>
+                <li className="hero-list">
+                  Get Massaged by Top and Talented Massage Therapists <br /> around
+                  your locality.
+                  <img className="hero-tick" src={heroTick} alt="" />
+                </li>
+                <li className="hero-list">
+                  Licensed professionals that heals the pain in your <br /> body and
+                  help you reduce stress?
+                  <img className="hero-tick" src={heroTick} alt="" />
+                </li>
+                <li className="hero-list" style={{whiteSpace: "nowrap"}}>
+                  Makes your search for real massage ends?
+                  <img className="hero-tick" src={heroTick} alt="" />
+                </li>
+                <li className="hero-list">
+                  Leave it to us we got you covered, we built a uber <br /> for
+                  masseurs
+                </li>
+              </ul>
+            </div>
+            <div className="book-btn">
+                <button className="home-hero-button">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={"https://wa.me/message/YNQGXF43I7LEA1"}
+                >
+                  Book a session
                 </a>
-           </div>
-            <AnimationOnScroll animateIn="animate__bounceIn" initiallyVisible="true">
-                <div className='right'>
-                    {/* <img className='main-image' src={HeroImage} alt="" />  */}
-                    <img className='main-image' src={mainHome} alt="" /> 
-                    <img className="mobile-img" src={mainHomeMobile} />
-                    <img className='ellipse' src={HeroBg2} alt="" />
-                    <img className='sparkle' src={HeroBg4} alt="" />
-                </div>
-           </AnimationOnScroll>   
+                </button>
+            </div>
+          </div>
+          <AnimationOnScroll animateIn="animate__bounceIn" initiallyVisible="true">
+            <div className="right">
+              {/* <img className='main-image' src={HeroImage} alt="" />  */}
+              <img className="main-image" src={mainHome} alt="" />
+              <img className="mobile-img" src={mainHomeMobile} alt="" />
+              <img className="ellipse" src={HeroBg2} alt="" />
+              <img className="sparkle" src={HeroBg4} alt="" />
+            </div>
+          </AnimationOnScroll>
         </div>
-    );
-}
- 
+  );
+};
+
 export default HeroSection;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,54 +1,95 @@
-import React, { useState } from 'react'
-import "./index.scss"
-import { BsChevronDown, } from 'react-icons/bs'
-import { AiOutlineMenu,} from 'react-icons/ai'
-import {FaTimes } from 'react-icons/fa'
-import Logo from "../../assets/logo2.png"
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./index.scss";
+import { BsChevronDown } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FaTimes } from "react-icons/fa";
+import Logo from "../../assets/logo2.png";
+import { Link } from "react-router-dom";
+
+const navigation = [
+  { name: "Home", href: "/" },
+  { name: "Blog", href: "/blog" },
+  { name: "About Us", href: "/about" },
+];
 
 const NavBar = () => {
-    const [toggle, setToggle] = useState(false);
-    // const [navbar, setNavbar] = useState(false);
+  const [toggle, setToggle] = useState(false);
+const [activePage, setActivePage] = useState("/")
+  // const [navbar, setNavbar] = useState(false);
 
-        const handleClick = () => {
-        setToggle(!toggle);
-        };
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
 
-    return (  
-        <div className='nav-container'>
-            <div className="navbar" >
-                <div className='logo'>
-                    <Link to="/" style={{ textDecoration: 'none' }}>
-                        <li><img src={Logo} alt="" /></li>
-                    </Link>
-                </div>
-                <div className={toggle ? "nav-menu active" : "nav-menu"}>
-                <Link to="/" style={{ textDecoration: 'none' }}>
-                    <li className='home nav-item'>Home</li>
-                </Link>
-                <Link to="/blog" style={{ textDecoration: 'none' }}>
-                    <li className='nav-item'>Blog</li>
-                </Link>
-                <Link to="/about" style={{ textDecoration: 'none' }}>
-                    <li className='nav-item'>About Us</li>
-                </Link>
-                    {/* <Link to="/join-us" style={{ textDecoration: 'none' }}>
-                        <li className='join nav-item'>                
-                        <span>
-                            <button>
-                            Are you a therapist ? Join Us
-                        </button></span>
-                        </li>
-                    </Link> */}
-                    
-                </div>
-                
-                <div className="nav-icon" onClick={handleClick}>
-                    {toggle ? <FaTimes /> : <AiOutlineMenu />}
-                </div>
-            </div>
+  const manageClick = (href) => {
+    setActivePage(href);
+  };
+
+  return (
+    <div className="nav-container">
+      <div className="navbar">
+        <div className="logo">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li>
+              <img src={Logo} alt="" />
+            </li>
+          </Link>
         </div>
-    );
-}
- 
+        
+        <div className={toggle ? "nav-menu active" : "nav-menu"}>
+          <div className="links">
+            
+              {navigation.map((item) => (
+                <NavLink
+                  key={item.name}
+                  to={item.href}
+                  onClick={() => manageClick(item.href)}
+                  className={item.href === activePage ? "nav-link active" : "nav-link"}
+                >
+                  {item.name}
+                </NavLink>
+              ))}
+          </div>
+          
+          <li className="join nav-item">
+            Are you a therapist?
+            <NavLink to="/join-us" style={{ textDecoration: "none" }}>
+              <span>Join Us</span>
+            </NavLink>
+          </li>
+        </div>
+
+        <div className="nav-icon" onClick={handleClick}>
+          {toggle ? <FaTimes /> : <AiOutlineMenu />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default NavBar;
+// {/* <NavLink
+//             to="/"
+//             className={({ isActive }) => {}}
+//             activeClassName="activeItem"
+//             exact
+//           >
+//             <li className="linkbefore home nav-item">Home</li>
+//           </NavLink>
+
+//           <NavLink
+//             to="/blog"
+//             style={{ textDecoration: "none" }}
+//             activeClassName="activeItem"
+//           >
+//             <li className="linkbefore nav-item">Blog</li>
+//           </NavLink>
+
+//           <NavLink
+//             to="/about"
+//             style={{ textDecoration: "none" }}
+//             activeClassName="activeItem"
+//           >
+//             <li className="linkbefore nav-item">About Us</li>
+//           </NavLink> */}
