6444f31d329c91c1d80f747df034e405f4b151d1
unknown <ademolababalola2003@gmail.com>
2023-02-05 18:59:53 +0100

    blog page

--- src/pages/Blog/BlogPage.jsx
+++ src/pages/Blog/BlogPage.jsx
@@ -1,37 +1,42 @@
-import React, { useState } from 'react'
-import "./index.scss"
-import { BsChevronDown } from 'react-icons/bs'
-import About from "../../assets/about.png"
-import BlogAwait from "../../assets/blogAwait.png"
+import React, { useState } from "react";
+import "./index.scss";
+import { BsChevronDown } from "react-icons/bs";
+import About from "../../assets/about.png";
+import wavingHand from "../../assets/wavingHand.svg";
+import BlogAwait from "../../assets/blogAwait.png";
 
 const BlogPage = () => {
+  return (
+    <div className="blog-page">
+      <div className="top-section">
+        <h1>
+          <span className="heading-dark">
+            Hello <img src={wavingHand} alt="" />, We are Zenrapy Blog.
+          </span>
+          <span className="heading-light">
+            {" "}
+            see our thoughts, stories and ideas
+          </span>
+        </h1>
+      </div>
 
-    return (  
-        <div className='blog-page'>
-            <div className='top-section'>
-                <p className='heading'>
-                    <span className='bold-heading-dark'>Hello 👋, We are <br/>
-                    Zenrapy Blog.</span><span className='bold-heading-light'> see our <br/> 
-                    thoughts, stories and ideas</span> 
-                </p>
-           </div>
-           
-           <div className='bottom-section'>
-                <h2>Page Under Construction</h2>
-                <div className='context'>
-                    <p>
-                        Hello, the page you are tying to view is under 
-                        construction and temporarily unavailable.
-                        The team is doing some work behind the scenes,<br/>
-                        do check back later.😊<br/>
-                        Thank you
-                    </p>
-                    <img src={BlogAwait} />
-                </div>
-           </div>
-           
+      <div className="bottom-section">
+        <h2>Page Under Construction</h2>
+        <div className="context-container">
+          <div className="context">
+            <p>
+              Hello, the page you are tying to view is under construction and
+              temporarily unavailable. The team is doing some work behind the
+              scenes, do check back later.😊 <br /> Thank you
+            </p>
+          </div>
+          <div className="context-image-container">
+            <img src={BlogAwait} alt="" />
+          </div>
         </div>
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default BlogPage;
